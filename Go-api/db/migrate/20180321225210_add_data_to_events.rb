35e4ce06f96fd84cab7acbf320897f19454e24b9
Chris Mauldin <Chrislovemauldin@gmail.com>
2018-03-21 16:02:59 -0700

    Updated events model

---
+++ Go-api/db/migrate/20180321225210_add_data_to_events.rb
@@ -0,0 +1,7 @@
+class AddDataToEvents < ActiveRecord::Migration[5.1]
+  def change
+    add_column :events, :latitude, :float
+    add_column :events, :longitude, :float
+    add_column :events, :address, :string
+  end
+end

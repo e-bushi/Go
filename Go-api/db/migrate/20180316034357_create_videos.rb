22e200d652c9e3d18f71b3bc1332cfda0f201345
kaichimomose <30785433+kaichimomose@users.noreply.github.com>
2018-03-20 20:17:17 -0700

    updated video model

--- Go-api/db/migrate/20180316034357_create_videos.rb
+++ Go-api/db/migrate/20180316034357_create_videos.rb
@@ -2,6 +2,9 @@ class CreateVideos < ActiveRecord::Migration[5.1]
   def change
     create_table :videos do |t|
       t.string :video_url
+      t.integer :likes
+      t.integer :views
+      t.string :duration
 
       t.timestamps
     end
